a85101662d4b8ea69683b5059d1ba34ce8a3a796
Ghustavh Ehm <ghustavh97@gmail.com>
2021-02-18 12:21:10 +0000

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ cp docker/entrypoint.sh entrypoint.sh
 
 # cd ${GITHUB_WORKSPACE}
 
-ls -a
+# ls -a
 
 # copy this container's enviroment variables into env.sh so we can use them in the main container
 export -p >env.sh
@@ -28,4 +28,4 @@ sed -i '1s/^/\#\!\/bin\/sh -l\n/' env.sh
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
 # pass ${INPUT_ARGS} to docker run to match what github actions does to the original docker run command
-docker build -t actions-package-update --build-arg SET_NODE_VERSION="$SET_NODE_VERSION" . && docker run actions-package-update ${INPUT_ARGS}
+docker build -t actions-package-update --build-arg SET_NODE_VERSION="$SET_NODE_VERSION" --build-arg GITHUB_WORKSPACE="$GITHUB_WORKSPACE" . && docker run actions-package-update -v "env.sh":"/env.sh" -v "entrypoint.sh":"/entrypoint.sh" ${INPUT_ARGS}
